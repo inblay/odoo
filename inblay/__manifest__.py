5ff715ccfc96597b28067fa40a51177c8429e23a
Francisco J. Luna Vázquez <fluna@vauxoo.com>
2019-05-29 16:17:16 -0500

    [ADD] Initialize the main module

---
+++ inblay/__manifest__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# Copyright Vauxoo 2019
+{
+    'name': "Inblay",
+    'summary': """Principal module for Inblay.""",
+    'author': 'Vauxoo',
+    'website': 'https://www.vauxoo.com',
+    'license': 'AGPL-3',
+    'category': 'Installer',
+    'version': '12.0.0.0.1',
+    'depends': [
+        'account',
+        'account_accountant',
+        'crm',
+        'fleet',
+        'helpdesk',
+        'hr',
+        'hr_appraisal',
+        'hr_expense',
+        'hr_holidays',
+        'hr_recruitment',
+        'l10n_mx_reports',
+        'l10n_mx_edi',
+        'project',
+        'purchase',
+        'sale_management',
+        'stock',
+        'survey',
+        'timesheet_grid',
+        'website_sale',
+        'website',
+    ],
+    'test': [
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'auto_install': True,
+}
