61c85f3ed88d48b3214f5fcd7097cde548e50aaf
Francisco J. Luna Vázquez <fluna@vauxoo.com>
2019-05-29 11:01:09 -0500

    [ADD] Initialize the main module

---
+++ inblay/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright Vauxoo 2019
+{
+    'name': "Inblay",
+    'summary': """Principal module for Inblay.""",
+    'author': 'Vauxoo',
+    'website': 'https://www.vauxoo.com',
+    'license': 'AGPL-3',
+    'category': 'Installer',
+    'version': '12.0.1.0.1',
+    'depends': ['base'],
+    'test': [
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'auto_install': False,
+}
