c6c319c8b1d4f411227b188beb0b24ffa2775e0d
Yanina Aular <yanina.aular@vauxoo.com>
2019-08-28 16:29:14 -0500

    [ADD] New feature sale price usd
    New 'Sale Price in USD' field on the product's form.
    The Sale Price of Odoo is calculated in base to Sale Price in USD daily with the last rate of dollar.
    Allow computation in pricelist based on Sale Price in USD.
    The sales price is calculated daily with the new rate
    The sales price is re-calculated if Sales Price in USD field is changed or setted.

--- inblay/models/product_template.py
+++ inblay/models/product_template.py
@@ -2,6 +2,7 @@
 # Copyright 2019 Vauxoo
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
+from odoo.addons import decimal_precision as dp
 from odoo import fields, models, api
 
 
@@ -12,6 +13,11 @@ class ProductTemplate(models.Model):
         'Internal Reference', compute='_compute_default_code',
         inverse='_set_default_code', required=False, store=True)
 
+    sale_price_usd = fields.Float(
+        'Sale Price in USD',
+        digits=dp.get_precision('Product Price'),
+        help="Sale price of the product in USD currency")
+
     @api.multi
     @api.returns('self', lambda value: value.id)
     def copy(self, default=None):
@@ -20,3 +26,22 @@ class ProductTemplate(models.Model):
         default['default_code'] = default.get('default_code', '.')
 
         return super(ProductTemplate, self).copy(default=default)
+
+    @api.onchange('sale_price_usd')
+    def _onchange_sale_price(self):
+        usd_currency = self.env.ref('base.USD')
+        company = self.env.user.company_id
+        local_currency = company.currency_id
+        pricelist = self.env.ref('product.list0')
+        for product in self:
+            local_currency_value = usd_currency._convert(
+                product.sale_price_usd, local_currency, company,
+                fields.Date.today())
+            product.list_price = local_currency_value
+
+            items = pricelist.item_ids.filtered(
+                lambda r: r.compute_price == 'fixed'
+                and r.product_tmpl_id == product)
+
+            for item in items:
+                item.fixed_price = product.sale_price_usd
